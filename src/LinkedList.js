e92a8e1c5d2d3a14267e966952ae05e8975446bb
Nilton Júnior <niltonssjr@gmail.com>
2023-05-16 00:09:15 -0300

    feat: insert feature

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -1,161 +1,175 @@
-const util = require('util')
+const util = require("util");
 
 export class Node {
-    constructor(val){
-        this.data = val
-        this.next = null
-    }
+  constructor(val) {
+    this.data = val;
+    this.next = null;
+  }
 }
 
-export class SinglyLinkedList{
+export class SinglyLinkedListErrorMessages {
+  static outOfBoundaries = "Index out of boundaries";
+}
 
-    #length = 0
-    #errorMessages = {
-        outOfBoundaries: 'Index out of boundaries'
+export class SinglyLinkedList {
+  #length = 0;
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.#length = 0;
+  }
+
+  get length() {
+    return this.#length;
+  }
+
+  clear() {
+    this.#length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  toArray() {
+    const list = [];
+
+    if (this.head) {
+      list.push(this.head.data);
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+        list.push(current.data);
+      }
     }
 
-    constructor(){
-        this.head = null
-        this.tail = null
-        this.#length =0
+    return list;
+  }
+
+  fromArray(valueList) {
+    this.clear();
+    valueList.forEach((item) => this.push(item));
+    return this.#length;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    this.#length++;
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+      this.tail = newNode;
     }
 
-    get length(){
-        return this.#length
+    return value;
+  }
+
+  pop() {
+    if (!this.head) return null;
+
+    if (this.#length == 1) {
+      const returnedValue = this.head.data;
+      this.head = null;
+      this.tail = null;
+      this.#length = 0;
+      return returnedValue;
     }
 
-    clear(){
-        this.#length = 0
-        this.head = null
-        this.tail = null
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
     }
 
-    toArray(){
-        const list = []
+    newTail.next = null;
+    this.tail = newTail;
+    this.#length--;
+    return current.data;
+  }
 
-        if (this.head){
-            list.push(this.head.data)
-            let current = this.head
-            while(current.next){
-                current = current.next
-                list.push(current.data)
-            }
-        }
+  shift() {
+    if (!this.head) return null;
 
-        return list
+    if (this.#length == 1) return this.pop();
+
+    const value = this.head.data;
+    this.head = this.head.next;
+    this.#length--;
+    return value;
+  }
+
+  unshift(value) {
+    if (!this.head) return this.push(value);
+
+    const newNode = new Node(value);
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.#length++;
+
+    return value;
+  }
+
+  #retrieveTargetAndPriorNodes(targetIndex) {
+    const response = {
+      current: null,
+      prior: null,
+    };
+
+    if (this.length == 0) return response;
+
+    if (targetIndex >= this.length || targetIndex < 0) return response;
+
+    let index = 0;
+    response.current = this.head;
+
+    while (index < targetIndex) {
+      response.prior = response.current;
+      response.current = response.current.next;
+      index++;
     }
 
-    fromArray( valueList ){
-        this.clear()
-        valueList.forEach(
-            item => this.push(item)
-        )
-        return this.#length
+    return response;
+  }
+
+  get(index) {
+    return this.#retrieveTargetAndPriorNodes(index).current?.data || null;
+  }
+
+  set(index, value) {
+    const elementToBeUpdated = this.#retrieveTargetAndPriorNodes(index).current;
+    if (!elementToBeUpdated) {
+      throw new Error(SinglyLinkedListErrorMessages.outOfBoundaries);
+      return;
+    }
+    elementToBeUpdated.data = value;
+  }
+
+  insert(index, value) {
+    if (this.length == 0 || index < 0 || index > this.length - 1) {
+      throw new Error(SinglyLinkedListErrorMessages.outOfBoundaries);
     }
 
-    push(value){
+    if (index == 0) return this.unshift(value);
 
-        const newNode = new Node(value)
-        this.#length ++
+    if (index == this.length - 1) return this.push(value);
 
-        if (!this.head){
-            this.head = newNode
-            this.tail = this.head
-        } else {
-            let current = this.head
-            while(current.next){
-                current = current.next
-            }
-            current.next = newNode
-            this.tail = newNode
-        }
+    const { current, prior } = this.#retrieveTargetAndPriorNodes(index);
 
-        return value
-    }
+    const newNode = new Node(value);
 
-    pop(){
-        if (!this.head) return null
-        
-        if (this.#length == 1){
-            const returnedValue = this.head.data
-            this.head = null
-            this.tail = null
-            this.#length = 0
-            return returnedValue            
-        }
+    prior.next = newNode;
+    newNode.next = current;
+    this.#length++;
+  }
 
-        let current = this.head
-        let newTail = current
-
-        while(current.next){
-            newTail = current
-            current = current.next
-        }
-
-        newTail.next = null
-        this.tail = newTail
-        this.#length --
-        return current.data
-    }
-
-    shift(){
-        if (!this.head) return null
-
-        if (this.#length == 1) return this.pop()
-
-        const value = this.head.data
-        this.head = this.head.next
-        this.#length --
-        return value
-    }
-
-    unshift(value){
-        if (!this.head) return this.push(value)
-
-        const newNode = new Node(value)
-
-        newNode.next = this.head
-        this.head = newNode
-        this.#length++
-
-        return value
-    }
-
-    #retrieveTargetAndPriorNodes(targetIndex){
-        const response = {
-            current: null,
-            prior: null
-        }
-
-        if (this.length == 0) return response
-
-        if (targetIndex >= this.length || targetIndex < 0) return response
-
-        let index = 0
-        response.current = this.head
-
-        while(index < targetIndex){
-            response.prior = response.current
-            response.current = response.current.next
-            index++
-        }
-
-        return response
-    }
-
-    get(index) {
-        return this.#retrieveTargetAndPriorNodes(index).current?.data || null
-    }
-
-    set(index, value){
-        const elementToBeUpdated = this.#retrieveTargetAndPriorNodes(index).current
-        if (!elementToBeUpdated){
-            throw new Error(this.#errorMessages.outOfBoundaries)
-            return
-        }
-        elementToBeUpdated.data = value
-    }
-    insert(index, value){}
-    remove(index){}
-
+  remove(index) {}
 }
--- src/singly-linked-list.spec.js
+++ src/singly-linked-list.spec.js
@@ -1,235 +1,305 @@
-import { SinglyLinkedList } from './LinkedList'
-const util = require('util')
+import { SinglyLinkedList, SinglyLinkedListErrorMessages } from "./LinkedList";
+const util = require("util");
 
-const generateRandomNumber = () => Math.ceil(Math.random() * 20)
-const generateListOfNumbers = (amount = 1) => Array.from(Array(amount).keys()).map( generatedNumber => generateRandomNumber())
+const generateRandomNumber = ({ minimum = 0, maximum = 20 } = {}) =>
+  minimum + Math.ceil(Math.random() * (maximum - minimum));
+const generateListOfNumbers = (amount = 1) =>
+  Array.from(Array(amount).keys()).map((generatedNumber) =>
+    generateRandomNumber()
+  );
 
-describe('Linked List', () => {
-    let list
+describe("Linked List", () => {
+  let list;
 
-    beforeEach( () => {
-        list = new SinglyLinkedList
-    })
-    afterEach( () => {
-        list = null
-    })
+  beforeEach(() => {
+    list = new SinglyLinkedList();
+  });
+  afterEach(() => {
+    list = null;
+  });
 
-    it('To Array - empty lists', () => {
-        expect(list.toArray()).toEqual([])
-        expect(list.length).toEqual(0)
-    })
+  it("To Array - empty lists", () => {
+    expect(list.toArray()).toEqual([]);
+    expect(list.length).toEqual(0);
+  });
 
-    it('Push first item', () => {
-        const randomNumber = generateRandomNumber()
-        list.push(randomNumber)
-        expect(list.toArray()).toEqual([randomNumber])
-        expect(list.length).toEqual(1)
-    })
+  it("Push first item", () => {
+    const randomNumber = generateRandomNumber();
+    list.push(randomNumber);
+    expect(list.toArray()).toEqual([randomNumber]);
+    expect(list.length).toEqual(1);
+  });
 
-    it('Push second Item', () => {
-        const testNumbers = generateListOfNumbers(2)
-        list.fromArray(testNumbers)
-        expect(list.toArray()).toEqual(testNumbers)
-        expect(list.length).toEqual(2)
-    })
+  it("Push second Item", () => {
+    const testNumbers = generateListOfNumbers(2);
+    list.fromArray(testNumbers);
+    expect(list.toArray()).toEqual(testNumbers);
+    expect(list.length).toEqual(2);
+  });
 
-    it('Push several itens', () => {
+  it("Push several itens", () => {
+    const amount = generateRandomNumber();
+    const testNumbers = generateListOfNumbers(amount);
+    list.fromArray(testNumbers);
+    expect(list.toArray()).toEqual(testNumbers);
+    expect(list.length).toEqual(amount);
+  });
 
-        const amount = generateRandomNumber()
-        const testNumbers = generateListOfNumbers(amount)
-        list.fromArray(testNumbers)
-        expect(list.toArray()).toEqual(testNumbers)
-        expect(list.length).toEqual(amount)
-    })
+  it("Pop from empty list", () => {
+    const popedNumber = list.pop();
+    expect(popedNumber).toEqual(null);
+  });
 
-    it('Pop from empty list', () => {
-        const popedNumber = list.pop()
-        expect(popedNumber).toEqual(null)
-    })
+  it("Pop with one element", () => {
+    const testNumber = generateRandomNumber();
+    list.push(testNumber);
+    const extractedNumber = list.pop();
+    expect(extractedNumber).toEqual(testNumber);
+    expect(list.length).toEqual(0);
+  });
 
-    it('Pop with one element', () => {
-        const testNumber = generateRandomNumber()
-        list.push(testNumber)
-        const extractedNumber = list.pop()
-        expect(extractedNumber).toEqual(testNumber)
-        expect(list.length).toEqual(0)
-    })
+  it("Pop from fulfilled list", () => {
+    const testNumbers = generateListOfNumbers(5);
+    list.fromArray(testNumbers);
+    const lastNumber = testNumbers.pop();
+    const popedNumber = list.pop();
+    expect(list.toArray()).toEqual(testNumbers);
+    expect(popedNumber).toEqual(lastNumber);
+  });
 
-    it('Pop from fulfilled list', () => {
-        const testNumbers = generateListOfNumbers(5)
-        list.fromArray(testNumbers)
-        const lastNumber = testNumbers.pop()
-        const popedNumber = list.pop()
-        expect(list.toArray()).toEqual(testNumbers)
-        expect(popedNumber).toEqual(lastNumber)
-    })
+  it("Load list from array", () => {
+    const count = generateRandomNumber();
+    const testNumbers = generateListOfNumbers(count);
+    list.fromArray(testNumbers);
+    expect(list.length).toEqual(count);
+    expect(list.toArray()).toEqual(testNumbers);
+  });
 
-    it('Load list from array', () => {
-        const count = generateRandomNumber()
-        const testNumbers = generateListOfNumbers(count)
-        list.fromArray(testNumbers)
-        expect(list.length).toEqual(count)
-        expect(list.toArray()).toEqual(testNumbers)
-    })
+  it("Shift from empty list", () => {
+    const extractedNumber = list.shift();
+    expect(extractedNumber).toBe(null);
+    expect(list.length).toBe(0);
+  });
 
-    it('Shift from empty list', () => {
-        const extractedNumber = list.shift()
-        expect(extractedNumber).toBe(null)
-        expect(list.length).toBe(0)
-    })
+  it("Shift from one element list", () => {
+    const generatedNumber = generateRandomNumber();
+    list.push(generatedNumber);
+    const extractedNumber = list.shift();
+    expect(extractedNumber).toEqual(generatedNumber);
+    expect(list.length).toEqual(0);
+  });
 
-    it('Shift from one element list', () => {
-        const generatedNumber = generateRandomNumber()
-        list.push(generatedNumber)
-        const extractedNumber = list.shift()
-        expect(extractedNumber).toEqual(generatedNumber)
-        expect(list.length).toEqual(0)
-    })
+  it("Shift from some elements list", () => {
+    const count = generateRandomNumber();
+    const testNumbers = generateListOfNumbers(count);
+    list.fromArray(testNumbers);
+    const firstNumber = testNumbers.shift();
+    const extractedNumber = list.shift();
+    expect(list.toArray()).toEqual(testNumbers);
+    expect(list.length).toEqual(count - 1);
+    expect(extractedNumber).toEqual(firstNumber);
+  });
 
-    it('Shift from some elements list', () => {
-        const count = generateRandomNumber()
-        const testNumbers = generateListOfNumbers(count)
-        list.fromArray(testNumbers)
-        const firstNumber = testNumbers.shift()
-        const extractedNumber = list.shift()
-        expect(list.toArray()).toEqual(testNumbers)
-        expect(list.length).toEqual(count - 1)
-        expect(extractedNumber).toEqual(firstNumber)
-    })
+  it("Unshift into an empty list", () => {
+    const testNumber = generateRandomNumber();
+    list.unshift(testNumber);
+    expect(list.length).toEqual(1);
+    expect(list.toArray()).toEqual([testNumber]);
+  });
 
-    it('Unshift into an empty list', () => {
-        const testNumber = generateRandomNumber()
-        list.unshift(testNumber)
-        expect(list.length).toEqual(1)
-        expect(list.toArray()).toEqual([testNumber])
-    })
+  it("Unshift into a fulfilled list", () => {
+    const count = generateRandomNumber();
+    const testNumbers = generateListOfNumbers(count);
+    list.fromArray(testNumbers);
 
-    it('Unshift into a fulfilled list', () => {
-        const count = generateRandomNumber()
-        const testNumbers = generateListOfNumbers(count)
-        list.fromArray(testNumbers)
-        
-        const numberToBeUnshift = generateRandomNumber()
-        testNumbers.unshift(numberToBeUnshift)
+    const numberToBeUnshift = generateRandomNumber();
+    testNumbers.unshift(numberToBeUnshift);
 
-        list.unshift(numberToBeUnshift)
+    list.unshift(numberToBeUnshift);
 
-        expect(list.length).toEqual(count + 1)
-        expect(list.toArray()).toEqual(testNumbers)
-    })
+    expect(list.length).toEqual(count + 1);
+    expect(list.toArray()).toEqual(testNumbers);
+  });
 
-    it('Clear a list', () => {
-        const count = generateRandomNumber()
-        const testNumbers = generateListOfNumbers(count)
-        list.fromArray(testNumbers)    
-        
-        list.clear()
-        expect(list.toArray()).toEqual([])
-        expect(list.length).toEqual(0)
-    })
+  it("Clear a list", () => {
+    const count = generateRandomNumber();
+    const testNumbers = generateListOfNumbers(count);
+    list.fromArray(testNumbers);
 
-    it('Length property unprotected to read', () => {
-        const randomNumber = generateRandomNumber()
-        list.push(randomNumber)
-        expect(list.length).toEqual(1)
-    })
+    list.clear();
+    expect(list.toArray()).toEqual([]);
+    expect(list.length).toEqual(0);
+  });
 
-    it('Length property protected to write', () => {
-        const randomNumber = generateRandomNumber()
-        list.push(randomNumber)
-        expect.assertions(1)
-        try {
-            list.length = randomNumber + 5
-        } catch (error){
-            expect(list.length).toBe(1)
-        }
-    })
+  it("Length property unprotected to read", () => {
+    const randomNumber = generateRandomNumber();
+    list.push(randomNumber);
+    expect(list.length).toEqual(1);
+  });
 
-    it('Get element by index from an empty list', () => {
-        const randomIndex = generateRandomNumber()
-        const extractedNumber = list.get(randomIndex)
-        expect(extractedNumber).toBeNull()
-    })
+  it("Length property protected to write", () => {
+    const randomNumber = generateRandomNumber();
+    list.push(randomNumber);
+    expect.assertions(1);
+    try {
+      list.length = randomNumber + 5;
+    } catch (error) {
+      expect(list.length).toBe(1);
+    }
+  });
 
-    it('Get element by index with index out of boundaries', () => {
-        const count = generateRandomNumber()
-        const testNumbers = generateListOfNumbers(count)
-        list.fromArray(testNumbers)   
+  it("Get element by index from an empty list", () => {
+    const randomIndex = generateRandomNumber();
+    const extractedNumber = list.get(randomIndex);
+    expect(extractedNumber).toBeNull();
+  });
 
-        const indexOutForBoundares = generateRandomNumber() + count
-        const extractedNumber = list.get(indexOutForBoundares)
-        expect(extractedNumber).toBeNull()        
-    })
+  it("Get element by index with index out of boundaries", () => {
+    const count = generateRandomNumber();
+    const testNumbers = generateListOfNumbers(count);
+    list.fromArray(testNumbers);
 
-    it('Get element by index', () => {
-        const count = generateRandomNumber()
-        const testNumbers = generateListOfNumbers(count)
-        list.fromArray(testNumbers)   
-        
-        const randomExistingIndex = Math.floor(Math.random() * count)
-        const extractedNumber = list.get(randomExistingIndex)
+    const indexOutForBoundares = generateRandomNumber() + count;
+    const extractedNumber = list.get(indexOutForBoundares);
+    expect(extractedNumber).toBeNull();
+  });
 
-        expect(extractedNumber).toEqual(list.toArray()[randomExistingIndex])
-    })
+  it("Get element by index", () => {
+    const count = generateRandomNumber();
+    const testNumbers = generateListOfNumbers(count);
+    list.fromArray(testNumbers);
 
-    it('Set element by index from an empty list', () => {
-        const randomIndex = generateRandomNumber()
-        const randomValue = generateRandomNumber()
-        expect.assertions(1)        
-        try {
-            list.set(randomIndex, randomValue)
-        } catch (error) {
-            expect(error.message).toBe('Index out of boundaries')
-        }
-    })
+    const randomExistingIndex = Math.floor(Math.random() * count);
+    const extractedNumber = list.get(randomExistingIndex);
 
-    it('Set element by a nonexistent index', () => {
-        const count = generateRandomNumber()
-        const testNumbers = generateListOfNumbers(count)
-        list.fromArray(testNumbers)   
+    expect(extractedNumber).toEqual(list.toArray()[randomExistingIndex]);
+  });
 
-        const indexOutForBoundares = generateRandomNumber() + count
-        const randomValue = generateRandomNumber()
+  it("Set element by index from an empty list", () => {
+    const randomIndex = generateRandomNumber();
+    const randomValue = generateRandomNumber();
+    expect.assertions(1);
+    try {
+      list.set(randomIndex, randomValue);
+    } catch (error) {
+      expect(error.message).toBe(SinglyLinkedListErrorMessages.outOfBoundaries);
+    }
+  });
 
-        expect.assertions(1)        
-        try {
-            list.set(indexOutForBoundares, randomValue)
-        } catch (error) {
-            expect(error.message).toBe('Index out of boundaries')
-        }     
-    })
+  it("Set element by a nonexistent index", () => {
+    const count = generateRandomNumber();
+    const testNumbers = generateListOfNumbers(count);
+    list.fromArray(testNumbers);
 
-    it('Set element by index', () => {
-        const count = generateRandomNumber()
-        const testNumbers = generateListOfNumbers(count)
-        list.fromArray(testNumbers)   
-        
-        const randomExistingIndex = Math.floor(Math.random() * count)
-        const randomValue = generateRandomNumber()
+    const indexOutForBoundares = generateRandomNumber() + count;
+    const randomValue = generateRandomNumber();
 
-        list.set(randomExistingIndex, randomValue)
+    expect.assertions(1);
+    try {
+      list.set(indexOutForBoundares, randomValue);
+    } catch (error) {
+      expect(error.message).toBe(SinglyLinkedListErrorMessages.outOfBoundaries);
+    }
+  });
 
-        expect(list.toArray()[randomExistingIndex]).toEqual(randomValue)
-    })
-    
-    
-    it.todo('Insert an element in an empty list')
-    
-    it.todo('Insert an element in the top of the list')
-    
-    it.todo('Insert an element in the bottom of the list')
-    
-    it.todo('Insert an element in the middle of the list')
+  it("Set element by index", () => {
+    const count = generateRandomNumber();
+    const testNumbers = generateListOfNumbers(count);
+    list.fromArray(testNumbers);
 
-    it.todo('Insert an element using a nonexistent index')
+    const randomExistingIndex = Math.floor(Math.random() * count);
+    const randomValue = generateRandomNumber();
 
-    it.todo('Remove an element from an empty list')
-    
-    it.todo('Remove an element from the top of the list')
-    
-    it.todo('Remove an element from the bottom of the list')
-    
-    it.todo('Remove an element from the middle of the list')
+    list.set(randomExistingIndex, randomValue);
 
-    it.todo('Remove an element using a nonexistent index')    
-})
+    expect(list.toArray()[randomExistingIndex]).toEqual(randomValue);
+  });
+
+  it("Insert an element in an empty list", () => {
+    const randomIndex = generateRandomNumber();
+    const randomNumber = generateRandomNumber();
+    expect.assertions(1);
+    try {
+      list.insert(randomIndex, randomNumber);
+    } catch (error) {
+      expect(error.message).toEqual(
+        SinglyLinkedListErrorMessages.outOfBoundaries
+      );
+    }
+  });
+
+  it("Insert an element using a nonexistent index", () => {
+    const count = generateRandomNumber();
+    const numbersList = generateListOfNumbers(count);
+    list.fromArray(numbersList);
+
+    const indexOutOfBoundaries = generateRandomNumber() + count;
+    const randomNumber = generateRandomNumber();
+    expect.assertions(1);
+
+    try {
+      list.insert(indexOutOfBoundaries, randomNumber);
+    } catch (error) {
+      expect(error.message).toEqual(
+        SinglyLinkedListErrorMessages.outOfBoundaries
+      );
+    }
+  });
+
+  it("Insert an element in the top of the list", () => {
+    const count = generateRandomNumber();
+    const numbersList = generateListOfNumbers(count);
+    list.fromArray(numbersList);
+
+    const randomNumber = generateRandomNumber();
+
+    numbersList.unshift(randomNumber);
+    list.insert(0, randomNumber);
+
+    expect(list.length).toEqual(count + 1);
+    expect(list.toArray()).toEqual(numbersList);
+  });
+
+  it("Insert an element in the bottom of the list", () => {
+    const count = generateRandomNumber();
+    const numbersList = generateListOfNumbers(count);
+    list.fromArray(numbersList);
+
+    const randomNumber = generateRandomNumber();
+
+    numbersList.push(randomNumber);
+    list.insert(count - 1, randomNumber);
+
+    expect(list.length).toEqual(count + 1);
+    expect(list.toArray()).toEqual(numbersList);
+  });
+
+  it("Insert an element in the middle of the list", () => {
+    const count = generateRandomNumber({ minimum: 3 });
+    const numbersList = generateListOfNumbers(count);
+    list.fromArray(numbersList);
+
+    const randomNumber = generateRandomNumber();
+
+    const randomValidIndex = generateRandomNumber({
+      minimum: 1,
+      maximum: count - 2,
+    });
+
+    numbersList.splice(randomValidIndex, 0, randomNumber);
+    list.insert(randomValidIndex, randomNumber);
+
+    expect(list.length).toEqual(count + 1);
+    expect(list.toArray()).toEqual(numbersList);
+  });
+
+  it.todo("Remove an element from an empty list");
+
+  it.todo("Remove an element from the top of the list");
+
+  it.todo("Remove an element from the bottom of the list");
+
+  it.todo("Remove an element from the middle of the list");
+
+  it.todo("Remove an element using a nonexistent index");
+});
